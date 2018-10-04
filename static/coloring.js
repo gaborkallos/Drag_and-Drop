fb00ab8b38c2256cb910d865740fa158a7d4a835
gaborkallos <gaborkallos@gmail.com>
2018-10-04 14:31:36 +0200

    color changing fixed

--- static/coloring.js
+++ static/coloring.js
@@ -4,14 +4,14 @@ function getRandomInt(min, max) {
 
 function changeBackground() {
     let element = document.getElementsByClassName('card');
-    let r = getRandomInt(0, 255);
-    let g = getRandomInt(0, 255);
-    let b = getRandomInt(0, 255);
     for (let i = 0; i < element.length; i++) {
         element[i].addEventListener('mouseover', function () {
+                let r = getRandomInt(0, 255);
+                let g = getRandomInt(0, 255);
+                let b = getRandomInt(0, 255);
                 this.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
             }
-        )
+        );
         element[i].addEventListener('mouseout', function () {
                 this.style.backgroundColor = 'white';
             }
